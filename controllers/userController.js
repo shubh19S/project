8e259a34e51f9cb640ece5436a41b3d259a7e36c
mobikasa11 <109499258+mobikasa11@users.noreply.github.com>
2022-07-18 15:02:08 +0530

    main

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,3 @@
-const user = require("../models/user");
 
 const registerUser = async (req, res) => {
   try {
--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const db = require('./config/db')
-const userRoutes = require('./routes/userRoutes')
-dotenv.config
+const express = require("express");
+const dotenv = require("dotenv");
+const db = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+dotenv.config;
 
-const app = express()
+const app = express();
 
+app.use("api/v1/user", userRoutes);
 
-app.use('api/v1/user',userRoutes)
+db.sync()
+  .then((result) => console.log("sync success"))
+  .catch((err) => console.log("sync error ", err.message));
 
-db
-    .sync()
-    .then(result=>console.log('sync success'))
-    .catch(err=>console.log('sync error ', err.message))
-
-const PORT = process.env.PORT||5000
-app.listen(PORT,()=>{
-    console.log(`server started on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
