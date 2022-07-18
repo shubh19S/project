06b27421d66b7c2489fc6bbf7718459cc26bc592
mobikasa11 <109499258+mobikasa11@users.noreply.github.com>
2022-07-18 11:34:17 +0530

    database connected

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv')
+dotenv.config()
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  port:process.env.DB_PORT,
+  dialect: 'mysql'
+});
+const checkAuth =async()=>{
+ try {
+    await sequelize.authenticate()
+    console.log("auth success")
+   
+ } catch (err) {
+    console.log('Error occured ', error)
+    
+ }
+}
+
+checkAuth()
+module.exports = sequelize
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hi')
+const express = require('express')
+const dotenv = require('dotenv')
+const db = require('./config/db')
+dotenv.config
+
+const app = express()
+
+db
+    .sync()
+    .then(result=>console.log('sync success'))
+    .catch(err=>console.log('sync error ', err.message))
+
+
+const PORT = process.env.PORT||5000
+app.listen(PORT,()=>{
+    console.log(`server started on port ${PORT}`)
+})
