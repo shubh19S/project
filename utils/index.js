9cb781df7d975d72194347de8be66aaeddfa4748
Mobikasa-MAC-MINI <mobikasa-mac-mini@Mobikasa-MAC-MINIs-Mac-mini.local>
2022-07-22 18:49:16 +0530

    otp generater and expiry time utils

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,10 @@
 const hashUtil = require('./hashUtil')
-
+const otpGenerator = require('./otpGenerator')
+const addMinutes = require('./addMinutes')
 module.exports = {
-   hashUtil
+   hashUtil,
+   otpGenerator,
+   addMinutes
 };
 
+s
