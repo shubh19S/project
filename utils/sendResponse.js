983206daa15d7708d21ee31ba2dbc5e375214816
“Ravindra” <ravindra@mobikasa.com>
2022-07-26 16:33:19 +0530

    --amend

--- utils/sendResponse.js
+++ utils/sendResponse.js
@@ -1,3 +1,10 @@
+/** 
+ * @method
+ * @param {object} app is instance of express application class. 
+ * This function is used to send common  json  API response. 
+ * We can call @method sendResponse() method on @see application.
+ * 
+ */
 module.exports = (app) => {
    app.response.sendResponse = function( message, data = undefined ){
     this.json({
